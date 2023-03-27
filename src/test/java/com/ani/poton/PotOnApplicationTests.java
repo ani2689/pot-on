eb6d75c1f3a0bd695ccb0f35efc7a52327a264be
ani2689 <ojno7777@gmail.com>
2023-03-27 20:01:46 +0900

    :tada: 프로젝트 생성

---
+++ src/test/java/com/ani/poton/PotOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.poton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
